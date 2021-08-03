9c1158f0e25b6afd1a2313532edbf966943b4d20
Kace Cottam <kace.cottam@wsu.edu>
2021-08-03 11:23:03 -0700

    added migrate from curl sh

---
+++ migrate_from_curl.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+git clone https://github.com/KaceCottam/IndexBot5 -o IndexBot5
+cp ./roles.db IndexBot5/old_roles.db -v
+source ./.env
+cd IndexBot5
+npm install --production
+cp ./default_config.json ./config.json
+node -e "var a=require('./config.json');a.TOKEN='$BOT_TOKEN';require('fs').writeFileSync('./config.json',JSON.stringify(a,null,4))"
+for gid in $(sqlite3 old_roles.db .tables | sed -e 's/guild_//g'); do
+    ./migrate.sh old_roles.db $gid
+done
