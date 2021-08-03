99609dbc33d1c3613b8a731f90c6a44f7092368a
Kace Cottam <kace.cottam@wsu.edu>
2021-08-03 11:30:50 -0700

    Updated bot to allow for replit

--- index.js
+++ index.js
@@ -101,3 +101,13 @@ client.on('error', err => {
 })
 
 client.login(TOKEN)
+
+const express = require('express')
+const app = express()
+const port = 3000
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+})
